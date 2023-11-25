9a1728f7105efdedbfd1f9e22857001c0af76498
Pavel <shadow-hunter2000@yandex.ru>
2023-11-26 01:27:54 +0800

    Все задачи

---
+++ Zadacha_03/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task_03
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Random random = new Random();
+            float arraymin;
+            float arraymax;
+            float min = 0F;
+            float max = 10.0F;
+            float randomFloatInRange = min + (max - min) * (float)random.NextDouble();
+            float razn;
+            int size = 10;
+            float[] array = new float[size];
+            for (int i = 0; i < size; i++)
+            {
+                 array[i] = (float)random.NextDouble();
+            }
+
+            Console.WriteLine("Случайные вещественные числа:");
+
+            foreach (float number in array)
+            {
+                Console.WriteLine(number);
+            }
+            arraymin = array[0];
+            for (int i = 0; i < size; i++)
+            {
+                if(array[i] < arraymin )
+                {
+                    arraymin = array[i];
+                }
+            }
+            Console.Write($" Минимальное значение массива => {arraymin}");
+            arraymax = array[0];
+             for (int i = 0; i < size; i++)
+            {
+                if(array[i] > arraymax )
+                {
+                    arraymax = array[i];
+                }
+            }
+            Console.Write($" Максимальное значение массива => {arraymax}");
+            razn = arraymax-arraymin;
+            System.Console.WriteLine($"Разница между максимальным и минимальным значением => {razn}");
+
+        }
+    }
+}
+
---
+++ Zadacha_04/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task_04
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Random rand = new Random();
+            int size = 10;
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                 array[i] = rand.Next(1, 100001);
+            }
+
+            Console.WriteLine("Массив без изменений => ");
+
+            foreach (int number in array)
+            {
+                Console.WriteLine(number);
+            }
+            
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = array[size-1-i];
+            }
+            Console.WriteLine("Массив наоборот => ");
+
+            foreach (int number in array)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
+
+
